42832d77fcbff5ae5bdbde91ccd756ae9ea2dff4
Phuc Pham <phamphuc01072003@gmail.com>
2024-09-28 17:04:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(session({
     secret: secretKey,
